a9cd51ef72a0f4c415094284431a08108fd90a16
Mlemoney <117745987+Mlemoney@users.noreply.github.com>
2024-05-28 18:37:36 +0000

    Update ConnexionBase.java

--- src/main/java/dao/ConnexionBase.java
+++ src/main/java/dao/ConnexionBase.java
@@ -1,7 +1,30 @@
 package dao;
-
+import java.sql.*;
 public class ConnexionBase {
+	Connection COO=null;
+	public Statement st=null;
+	String user="root";
+	String MDP="";
+	String Url="jdbc:mysql://localhost/gestion_resto";
+	String drive="com.mysql.cj.jdbc.Driver";
 	public void conn() {
-		
+		try {
+			Class.forName(drive);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			COO= DriverManager.getConnection(Url, user, MDP);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			st=COO.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
