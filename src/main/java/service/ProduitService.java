2144008bdf133e51f3e983a8e1c9e7a345b9892f
Moustapha LY <moustaphaly1996@gmail.com>
2024-06-10 11:17:03 +0000

    mise a jour des requetes dans ProduitService

--- src/main/java/service/ProduitService.java
+++ src/main/java/service/ProduitService.java
@@ -1,5 +1,7 @@
 package service;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 
 import dao.ConnexionBase;
@@ -8,18 +10,28 @@ public class ProduitService {
 	
 	public int quantiteVendueParProduit(int id) {
 		int quantite = 0;
+		ResultSet rs = null;
 		
 		// Connexion bdd
 		ConnexionBase cb = new ConnexionBase();
-		cb.connect();
+		cb.conn();
 		
 		// Requete SQL
 		String requete = "select (select count(article) from VenteC where article.id=" +id+ ") + "
-				+ "(select count(article) from VenteC where article.id=" +id+ ") ";
+				+ "(select count(article) from VenteP where article.id=" +id+ ") as qte";
 		
 		// Executer la requete
-		quantite = cb.st.executeQuery(requete);
-		
+		try {
+			rs = cb.st.executeQuery(requete);
+			
+			if(rs != null) {
+				quantite = rs.getInt("qte");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		return quantite;
 	}
 	
@@ -27,14 +39,23 @@ public class ProduitService {
 		int montant = 0;
 		
 		// Connexion bdd
-		ConnexionBase cb = new ConnexionBase();
-		cb.connect();
+		ConnexionBase cb = new ConnexionBase(); 
+		cb.conn();
 		
 		// Requete SQL
 		String requete = "select (select sum(pu) from VenteC) + (select sum(pu) from VenteC) as total_sum";
 		
 		// Executer la requete
-		montant = cb.st.executeQuery(requete);
+		try {
+			ResultSet rs = cb.st.executeQuery(requete);
+			
+			if(rs != null) {
+				montant = rs.getInt("total_sum");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return montant;
 	}
@@ -44,14 +65,23 @@ public class ProduitService {
 		
 		// Connexion bdd
 		ConnexionBase cb = new ConnexionBase();
-		cb.connect();
+		cb.conn();
 		
 		// Requete SQL
 		String requete = "select (select sum(pu) from VenteC where date>"+periodeDebut+" and date<"+periodeFin+") "
-				+ "+ (select sum(pu) from VenteC where date>"+periodeDebut+" and date<"+periodeFin+") as total_sum";
+				+ "+ (select sum(pu) from VenteP where date>"+periodeDebut+" and date<"+periodeFin+") as total_sum";
 		
 		// Executer la requete
-		montant = cb.st.executeQuery(requete);
+		try {
+			ResultSet rs = cb.st.executeQuery(requete);
+			
+			if (rs != null) {
+				montant = rs.getInt("total_sum");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return montant;
 	}
